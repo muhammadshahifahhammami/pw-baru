f213f594cc7d7d277d9603c55aa8afbcf2cc1c7a
muhammadshahifahhammami <shahifah0210@gmail.com>
2023-12-12 01:25:48 -0800

    pw baru

---
+++ praktik 11/parkir.js
@@ -0,0 +1,25 @@
+function hitungParkir(){
+    // daptkan nilai inputtan user
+    let masuk = document.getElementById("masuk").value
+    let keluar = document.getElementById("keluar").value
+
+    // dapatkan element untuk menampilkan hasil
+    let hasilDurasi = document.querySelector("#hasilDurasi")
+    let hasilBiaya = document.querySelector("#hasilBiaya")
+
+    // hitung durasi
+    let durasi = keluar - masuk
+    
+    // hitung biaya
+    let biaya = 3000
+
+    // setalah 2 jam pertama, tambahkan biaya perjamnya 1000
+    if(durasi > 2){
+        biaya += (durasi - 2) * 1000
+    }
+
+    console.log(biaya);
+
+    hasilBiaya.innerHTML = biaya
+    hasilDurasi.innerHTML = durasi
+}
